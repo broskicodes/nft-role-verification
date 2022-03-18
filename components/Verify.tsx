04e67d71941be33f8669e59c9407a24222f65c22
nibbus <iffynibba@gmail.com>
2022-03-17 20:52:32 -0400

    First commit

---
+++ components/Verify.tsx
@@ -0,0 +1,70 @@
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { 
+  Transaction, 
+  TransactionInstruction,
+  SystemProgram,
+  AccountMeta
+} from "@solana/web3.js";
+import hashlist from '../data/hashlist.json';
+
+export const Verify = () => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction, signMessage } = useWallet();
+
+  const verifyNftOwnership = async (isLedger) => {  
+    if(!connection){
+      return;
+    }
+  
+    if(!isLedger){
+      const msg = "Please sign to verify wallet ownership.";
+      const signedMsg = await signMessage(
+        new TextEncoder().encode(msg),
+      );
+  
+      console.log(signedMsg);
+    } else {
+      alert("You will be prompted to send a transaction to yourself to verify ownership.");
+      
+      const tx = new Transaction({
+        recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+        feePayer: publicKey,
+      });
+        
+      tx.add(
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: publicKey,
+          lamports: 1
+        }),
+      );
+  
+      const txId = await sendTransaction(
+        tx,
+        connection,
+      );
+  
+      console.log(txId);
+    }
+
+    const nftMetadatas = await Metadata.findDataByOwner(connection, publicKey);
+
+    let owner = false;
+    nftMetadatas.forEach((mtdt) => {
+      if(hashlist.includes(mtdt.mint)){
+        owner = true;
+      }
+    });
+
+    console.log(owner);
+    return owner;
+  }
+
+  return (
+    <div>
+      <button onClick={() => { verifyNftOwnership(false) }}>Verify</button>
+      <button onClick={() => { verifyNftOwnership(true) }}>Verify Ledger</button>
+    </div>
+  );
+};
---
+++ components/Wallet.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export const Wallet = ({ children }) => {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
